729ad87d227529b93ec765cfa14f6d9930ce9b56
iatirah19 <iatirah19@gmail.com>
2026-06-24 15:32:42 +0800

    done

--- sahabat_travel/admin/auth.php
+++ sahabat_travel/admin/auth.php
@@ -107,9 +107,8 @@ if (isset($_POST['login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Authentication</title>
-
+    <link rel="icon" type="image/png" href="../picture/LOGO.png">
     <link rel="stylesheet" href="auth.css">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
